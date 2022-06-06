424da1da4cd22fa20c6344fee875cf1bca532f8c
Carlos Moreira <carlosfwm@gmail.com>
2022-06-06 20:44:33 -0300

    chore: add jest configs and scripts

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -179,7 +179,7 @@ describe('SignUp Controller', () => {
       }
     }
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.statusCode).toBe(600)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
