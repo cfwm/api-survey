58083acfb7af70dffaab76f05cfcca25e1115793
Carlos Moreira <carlosfwm@gmail.com>
2022-07-19 22:32:06 -0300

    test: ensure AccountMongoRepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongodb-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    // await MongoHelper.connect(global.__MONGO_URI__);
+    // await MongoHelper.connect(process.env.__MONGO_URI__);
+    await MongoHelper.connect('mongodb://localhost:27017');
+  })
+  
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@email.com', 
+      password: 'any_password',
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(account.name)
+    expect(account.email).toBe(account.email)
+    expect(account.password).toBe(account.password)
+  })
+})
