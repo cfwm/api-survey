5c588f8252d44ff103722b1ee6b0cb9fcbfa4db3
Carlos Moreira <carlosfwm@gmail.com>
2022-07-19 21:47:06 -0300

    chore: fix jest configs for mongodb

--- src/infra/db/mongodb/helpers/mongodb-helper.ts
+++ src/infra/db/mongodb/helpers/mongodb-helper.ts
@@ -4,12 +4,16 @@ export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (uri: string): Promise<void> {
-     // this.client = await MongoClient.connect(global.__MONGO_URI__, {
-      // this.client = await MongoClient.connect(process.env.__MONGO_URI__, {
-      this.client = await MongoClient.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+    //  this.client = await MongoClient.connect(global.__MONGO_URI__, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+    //  this.client = await MongoClient.connect(process.env.__MONGO_URI__, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // });
+      
+    this.client = await MongoClient.connect(uri);
   },
 
   async disconnect (): Promise<void> {
